59b8c56c7dc639d743ef44cc9ae7f20bba060c3f
jotasie7e <41646290+jotasie7e@users.noreply.github.com>
2021-06-14 19:41:25 -0500

    Creacion Css, img, inc, paginas

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+  include 'inc/funciones/funciones.php';
+  include 'inc/templates/header.php';
+?>
+
+    <div class="contenedor">
+        <div class="barra">
+            <a class="logo" href="../pages/">
+                <h2 class="logo__nombre">
+                    Api<span class="logo__bold">Weather</span>
+                </h2>
+            </a>
+            <nav class="navegacion">
+                <a class="navegacion__enlace" href="registro.php">
+                    Registro
+                </a>
+                <a class="navegacion__enlace" href="login.php">
+                    Login
+                </a>
+            </nav>
+        </div>
+    </div>
+    <div class="texto">
+        <h1>Plataforma climatica</h1>
+        <h3 class="texto__titulo">Proporcionamos datos metereologicos de tu ciudad</h3>
+    </div>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,25 @@
+<?php
+  include 'inc/funciones/funciones.php';
+  include 'inc/templates/header.php';  
+?>
+  <div class="contenedor">
+    <div class="barra">
+      <a class="logo" href="index.php">
+        <h2 class="logo__nombre">
+          Api<span class="logo__bold">Weather</span>
+        </h2>
+      </a>
+      <nav class="navegacion">
+        <a class="navegacion__enlace" href="registro.php">
+          Registro
+        </a>
+        <a class="navegacion__enlace" href="login.php">
+          Login
+        </a>
+      </nav>
+    </div>
+  </div>
+  <div class="texto">
+  </div>
+</body>
+</html>
